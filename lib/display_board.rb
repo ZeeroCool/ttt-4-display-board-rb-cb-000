643fa21ac701041103f50f40ccf0b0d70ad07b63
David Z <davidzabran@gmail.com>
2018-07-21 03:04:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 
   board = [" "," "," "," "," "," "," "," "," "]
-
+  
 # Define display_board that accepts a board and prints
-def display_board
-puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
+def display_board(board)
+puts " #{board[0] } | #{board[1] } | #{board[2] } \n-----------\n #{board[3] } | #{board[4] } | #{board[5] } \n-----------\n #{board[6] } | #{board[7] } | #{board[8] } "
 end
 # out the current state.
